450cc3dbbf204986b68f1b6a811eb9e70fc46476
chaseunghun <shc0526@gmail.com>
2018-06-16 01:19:06 +0000

    Main.java

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,22 +1,24 @@
 package project;
+
 import java.util.Random;
 import java.util.Arrays;
+
 public class Main {
+   
     private Random random = new Random(System.nanoTime());
     
     public int[] generate() {
         int[] result = new int[6];
         
-        for(i=0; i<6; i++){
-            result[i] = random.nextint(45);
+        for(i=0; i<6; i++) {
+            result[i] = random.nextint(45)+1;
         }
         return result;
     }
 
     public static void main(String[] args) {
         int[] = result = new main().generate();
-        System.out.println("Array.toString(result));
-    }
+        System.out.println("Array.toString(result)");
     }
 
 }
