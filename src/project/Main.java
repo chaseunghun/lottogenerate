01b4c6b78bc6bd4b8899bcaac03dbbba1a9772d9
chaseunghun <shc0526@gmail.com>
2018-06-16 02:31:51 +0000

    mod

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,23 +2,34 @@ package project;
 
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 
-public class Main {
-   
+public class main {
     private Random random = new Random(System.nanoTime());
-    
     public int[] generate() {
-        int[] result = new int[6];
+        int[] result = new int[6];  
+        int index = 0;
+        Set<Integer> generated = new HashDet<>();
         
-        for(i=0; i<6; i++) {
-            result[i] = random.nextint(45)+1;
+        while(generated.size() < 6) {
+            int num = random.nextInt(45) +1;
+            
+            if(!contains(generated, num)) {
+                result[index++] = num; 
+                genetated.add(num);
+            }
         }
-        return result;
+        
+    return result;
     }
-
+    boolen contains(Set<Integer) generated, int num) {
+        return generated.contains(num);
+    }
     public static void main(String[] args) {
-        int[] = result = new main().generate();
-        System.out.println("Array.toString(result)");
+        int[] result = new main().generate();
+        System.out.println(Arrays.toString(result));
     }
-
 }
+
+
